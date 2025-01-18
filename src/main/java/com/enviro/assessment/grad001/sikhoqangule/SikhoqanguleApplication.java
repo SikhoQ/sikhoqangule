639bd8b08a4aeee379d0ce5c2dd1dbb4d6c4e142
Sikho Qangule <qangulesikho@gmail.com>
2025-01-18 17:18:55 +0200

    initialised application

---
+++ src/main/java/com/enviro/assessment/grad001/sikhoqangule/SikhoqanguleApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.sikhoqangule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SikhoqanguleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SikhoqanguleApplication.class, args);
+	}
+
+}
