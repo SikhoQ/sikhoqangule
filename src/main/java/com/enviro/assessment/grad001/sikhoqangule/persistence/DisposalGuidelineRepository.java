fc3d739f21b9168c341619497239859ce43653e0
Sikho Qangule <qangulesikho@gmail.com>
2025-01-24 13:14:16 +0200

    defined repositories for each entity

---
+++ src/main/java/com/enviro/assessment/grad001/sikhoqangule/persistence/DisposalGuidelineRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.sikhoqangule.persistence;
+
+import com.enviro.assessment.grad001.sikhoqangule.model.DisposalGuideline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuideline, Long> {
+}
---
+++ src/main/java/com/enviro/assessment/grad001/sikhoqangule/persistence/RecyclingTipRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.sikhoqangule.persistence;
+
+import com.enviro.assessment.grad001.sikhoqangule.model.RecyclingTip;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecyclingTipRepository extends JpaRepository<RecyclingTip, Long> {
+}
