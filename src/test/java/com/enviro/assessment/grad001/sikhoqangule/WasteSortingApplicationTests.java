8cb307e0aa76616c29f455532036cf7db1d545c0
Sikho Qangule <qangulesikho@gmail.com>
2025-01-25 00:45:25 +0200

    added unit tests for waste category CRD operations

--- src/test/java/com/enviro/assessment/grad001/sikhoqangule/WasteSortingApplicationTests.java
+++ src/test/java/com/enviro/assessment/grad001/sikhoqangule/WasteSortingApplicationTests.java
@@ -8,6 +8,8 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Objects;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -21,5 +23,33 @@ public class WasteSortingApplicationTests {
 		ResponseEntity<WasteCategory[]> response = restTemplate.getForEntity("/api/categories", WasteCategory[].class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 		assertThat(response.getBody()).isNotNull();
+	}
+
+	@Test
+	public void testCreateCategory() {
+		WasteCategory newCategory = new WasteCategory(null, "Metal", "Metal waste such as cans");
+		ResponseEntity<WasteCategory> response = restTemplate.postForEntity("/api/categories", newCategory, WasteCategory.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotNull();
+		assertThat(response.getBody().getName()).isEqualTo("Metal");
+	}
+
+	@Test
+	public void testGetCategoryById() {
+		WasteCategory newCategory = new WasteCategory(null, "Paper", "Normal paper");
+		ResponseEntity<WasteCategory> created = restTemplate.postForEntity("/api/categories", newCategory, WasteCategory.class);
+		Long createdId = Objects.requireNonNull(created.getBody()).getId();
+
+		ResponseEntity<WasteCategory> response = restTemplate.getForEntity("/api/categories/" + createdId, WasteCategory.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+		assertThat(response.getBody()).isNotNull();
+	}
+
+
+	@Test
+	public void testDeleteCategory() {
+		restTemplate.delete("/api/categories/1");
+		ResponseEntity<WasteCategory> response = restTemplate.getForEntity("/api/categories/1", WasteCategory.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
 	}
 }
