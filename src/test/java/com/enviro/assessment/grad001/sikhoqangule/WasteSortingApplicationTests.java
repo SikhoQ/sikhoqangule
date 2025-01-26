4d3076bf86b5918373e71d9488e05c5779c8c4c1
Sikho Qangule <qangulesikho@gmail.com>
2025-01-27 00:53:27 +0200

    moved endpoints tests to relevant package and restored application context loading tests

--- src/test/java/com/enviro/assessment/grad001/sikhoqangule/WasteSortingApplicationTests.java
+++ src/test/java/com/enviro/assessment/grad001/sikhoqangule/WasteSortingApplicationTests.java
@@ -1,55 +1,12 @@
 package com.enviro.assessment.grad001.sikhoqangule;
 
-import com.enviro.assessment.grad001.sikhoqangule.entity.WasteCategory;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 
-import java.util.Objects;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class WasteSortingApplicationTests {
 
-	@Autowired
-	private TestRestTemplate restTemplate;
-
 	@Test
-	public void testGetAllCategories() {
-		ResponseEntity<WasteCategory[]> response = restTemplate.getForEntity("/api/categories", WasteCategory[].class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).isNotNull();
-	}
-
-	@Test
-	public void testCreateCategory() {
-		WasteCategory newCategory = new WasteCategory("Metal", "Metal waste such as cans");
-		ResponseEntity<WasteCategory> response = restTemplate.postForEntity("/api/categories", newCategory, WasteCategory.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).isNotNull();
-		assertThat(response.getBody().getCategoryName()).isEqualTo("Metal");
-	}
-
-	@Test
-	public void testGetCategoryById() {
-		WasteCategory newCategory = new WasteCategory("Paper", "Normal paper");
-		ResponseEntity<WasteCategory> created = restTemplate.postForEntity("/api/categories", newCategory, WasteCategory.class);
-		Long createdId = Objects.requireNonNull(created.getBody()).getId();
-
-		ResponseEntity<WasteCategory> response = restTemplate.getForEntity("/api/categories/" + createdId, WasteCategory.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-		assertThat(response.getBody()).isNotNull();
-	}
-
-
-	@Test
-	public void testDeleteCategory() {
-		restTemplate.delete("/api/categories/1");
-		ResponseEntity<WasteCategory> response = restTemplate.getForEntity("/api/categories/1", WasteCategory.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+	void contextLoads() {
 	}
 }
